caabcff95e110e2610c208896f00617892299e15
Yasir Younas <yazyounas@gmail.com>
2022-10-06 13:07:19 +0100

    iterate using map method

--- iterators.js
+++ iterators.js
@@ -5,3 +5,21 @@ fruits.forEach(fruite => {
 console.log(`I want to eat a ${fruite}.`)
 });
 
+// iterate using .map method
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => {
+  return animal[0];
+
+});
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(smallNumber => {
+  return smallNumber / 100;
+});
+console.log(smallNumbers);
